8d0f8ea2b9718d8cba8ba2f2b1fb6d81c94b6487
Ranjay Kumar <Ranjay.Kumar@llamasoft.com>
2020-05-17 23:04:46 -0400

    added -y flag to docker-ce install

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -14,7 +14,7 @@ docker-compose --version
 
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce
+sudo yum install -y docker-ce
 sudo usermod -aG docker $(whoami)
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
